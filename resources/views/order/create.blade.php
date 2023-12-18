c986404beb936fdee780bd95eb2891462b9dd70a
shahzadali50 <108119561+shahzadali50@users.noreply.github.com>
2023-12-18 17:20:37 +0500

    add price in subtotal

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -142,10 +142,10 @@
                         <td>${prod_name}</td>/
                         <td>${prod_qty}</td>/
                         <td>${prod_price}</td>/
-                        <td>${prod_qty * prod_price}</td>/
+                        <td id="Total_Price">${prod_qty * prod_price}</td>/
                         <td><button type="button" onclick="removeIt(${prod_id})" value='' class="btn btn-light btn_delete"><i class="fa fa-times-circle text-danger" aria-hidden="true"></i></button></td>/
                         <td><input type="number" name="product_qty[]" value="${prod_qty}"  class="form-control" /></td>/
-                      <td>  <input type="number"  name="product_price[]" value="${prod_price }"  class="form-control" /></td>/
+                      <td>  <input type="number"  name="product_price[]" value="${prod_price }"  class="form-control product_price" /></td>/
                     </tr>
 
 
@@ -162,12 +162,6 @@
     function removeIt(id) {
         $('#add_items_' + id).remove();
     }
-    // $('.btn_delete').click(function() {
-    //     alert(val);
-    //     let val=$(this).val();
-
-
-    // });
 
 </script>
 <script>
@@ -237,6 +231,45 @@
         });
     });
 
+
+    // show value in subTotal
+$('.btn_Add_items').on('click', function() {
+    let subTotal=0;
+    var total_Price = $('.product_price');
+    console.log(total_Price);
+
+   $.each(total_Price, function(index, element) {
+    console.log(element);
+    subTotal+=parseFloat(element.value);
+});
+console.log(subTotal);
+    // alert(Total_Price)
+
+    //var subTotal = parseFloat($('#sub_total').text());
+    //subTotal += Total_Price;
+    $('#sub_total').text(subTotal);
+
+
+    // Check if the total_price_b is a valid number
+    // if (!isNaN(total_price_b)) {
+    //     // Get the current sub_total value
+    //     var subTotal = parseFloat($('#sub_total').text());
+
+    //     // Add the total_price_b to the sub_total
+    //     subTotal += total_price_b;
+
+    //     // Update the sub_total with the new value
+    //     $('#sub_total').text(subTotal.toFixed(2));
+
+    //     // Add a new row to the items table with the total_price_b value
+    //     var newRow = '<tr><td class="totalPriceB">' + total_price_b.toFixed(2) + '</td></tr>';
+    //     // $('#itemsTable').append(newRow);
+    // }
+
+    // // Clear the total_price_b input field
+    // $('#total_price_b').val('');
+});
+
 </script>
 
 
@@ -247,4 +280,3 @@
 
 
 @endpush
-
