88fdc473c2ec230600c03e32a22aa4313eab3d63
Shahzad Ali <108119561+shahzadali50@users.noreply.github.com>
2024-01-11 12:56:48 +0500

    update qty but input not update

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -15,16 +15,14 @@
                         <div class="col-md-4 col-sm-8">
                             <div class="form-group">
                                 <label for="simpleinput">Customer Name <span class="text-danger">*</span></label>
-                                <input type="text" name="customer_name" class="form-control" placeholder="Customer Name"
-                                    required>
+                                <input type="text" name="customer_name" class="form-control" placeholder="Customer Name" required>
                             </div>
 
                         </div>
                         <div class="col-md-4 col-sm-8 ">
                             <div class="form-group ">
                                 <label for="simpleinput">Phone No<span class="text-danger">*</span></label>
-                                <input type="number" name="customer_phone" class="form-control" placeholder="Phone No."
-                                    required>
+                                <input type="number" name="customer_phone" class="form-control" placeholder="Phone No." required>
                             </div>
 
                         </div>
@@ -55,24 +53,22 @@
                         <div class="col-lg-2 col-sm-1">
                             <div class="form-group">
                                 <label for="simpleinput">Qty</label>
-                                <input type="number" name="qty" id="quantityInput" class="form-control" >
+                                <input type="number" name="qty" id="quantityInput" class="form-control">
                             </div>
 
                         </div>
                         <div class="col-lg-1 col-sm-3">
                             <div class="form-group">
                                 <label for="simpleinput">Price</label>
-                                <input type="hidden" name="original_sale_rate" id="originalSaleRateInput"
-                                    class="form-control">
-                                <input type="number" name="sale_rate" id="saleRateInput" class="form-control" >
+                                <input type="hidden" name="original_sale_rate" id="originalSaleRateInput" class="form-control">
+                                <input type="number" name="sale_rate" id="saleRateInput" class="form-control">
                             </div>
 
                         </div>
                         <div class="col-lg-1 col-sm-3">
                             <div class="form-group">
                                 <label for="simpleinput"> Total</label>
-                                <input type="number" readonly name="total_price_b" id="total_price_b"
-                                    class="form-control" required>
+                                <input type="number" readonly name="total_price_b" id="total_price_b" class="form-control" required>
                             </div>
 
                         </div>
@@ -108,8 +104,7 @@
                                 <tr>
                                     <th>Sub Total</th>
                                     <td id="sub_total">00.00</td>
-                                    <input hidden id="sub_totalInput" type="number" name="sub_total" value="0"
-                                        class="form-control" />
+                                    <input hidden id="sub_totalInput" type="number" name="sub_total" value="0" class="form-control" />
                                 </tr>
                                 <tr>
                                     <th>Disc%</th>
@@ -120,8 +115,7 @@
                                 <tr>
                                     <th>Grand Total</th>
                                     <td id="grand_total">00.00</td>
-                                    <input hidden id="grand_totalInput" type="number" name="grand_total" value="0"
-                                        class="form-control" />
+                                    <input hidden id="grand_totalInput" type="number" name="grand_total" value="0" class="form-control" />
 
                                 </tr>
                             </table>
@@ -149,33 +143,48 @@
         let prod_name = $('.items_select :selected').text();
         let prod_qty = $('#quantityInput').val();
         let prod_price = $('#saleRateInput').val();
-        // alert(prod_id+" "+prod_name+" "+prod_qty+" "+prod_price);
+
         if (prod_id && prod_name && prod_qty && prod_price) {
-            $('#itemsTable').append(`
-                    <tr id="add_items_${prod_id}">/
+            // Check if product with the same ID already exists
+            let existingRow = $(`#add_items_${prod_id}`);
 
-                        <td>${prod_name}</td>/
-                        <td>${prod_qty}</td>/
-                        <td>${prod_price}</td>/
-                        <td>${prod_qty * prod_price}</td>/
-                        <td>
-                    <button type="button" onclick="removeIt(${prod_id})" class="btn btn-light btn_delete">
-                        <i class="fa fa-times-circle text-danger" aria-hidden="true"></i>
-                    </button> </td>  /
-                                            <td><input readonly  type="number" name="product_id[]" value="${prod_id}"  class="form-control" /> </td>/
-                                            <td><input readonly type="number" name="product_qty[]" value="${prod_qty}"  class="form-control" /> </td>/
-                                            <td>  <input type="number"  name="product_price[]" value="${prod_price }"  class="form-control product_price"/> </td>/
-                    </tr>
+            if (existingRow.length > 0) {
+                // Update quantity and total price
+                let existingQty = parseFloat(existingRow.find('td:nth-child(2)').text());
+                let existingPrice = parseFloat(existingRow.find('td:nth-child(3)').text());
 
+                existingRow.find('td:nth-child(2)').text(existingQty + parseFloat(prod_qty));
+                existingRow.find('td:nth-child(4)').text((existingQty + parseFloat(prod_qty)) * existingPrice);
+            } else {
+                // Add a new row
+                $('#itemsTable').append(`
+                <tr id="add_items_${prod_id}">
+                    <td>${prod_name}</td>
+                    <td>${prod_qty}</td>
+                    <td>${prod_price}</td>
+                    <td>${prod_qty * prod_price}</td>
+                    <td>
+                        <button type="button" onclick="removeIt(${prod_id})" class="btn btn-light btn_delete">
+                            <i class="fa fa-times-circle text-danger" aria-hidden="true"></i>
+                        </button>
+                    </td>
+                    <td><input readonly type="number" name="product_id[]" value="${prod_id}" class="form-control" /></td>
+                    <td><input readonly type="number" name="product_qty[]" value="${prod_qty}" class="form-control" /></td>
+                    <td><input type="number" name="product_price[]" value="${prod_price}" class="form-control product_price"/></td>
+                </tr>
+            `);
+            }
 
-                    `);
+            // Clear input fields
             $('.items_select').val('');
             $('#quantityInput').val('');
             $('#saleRateInput').val('');
+            
         } else {
             alert('Please fill in all required fields.');
         }
     });
+
 
     function removeIt(id) {
         $('#add_items_' + id).remove();
@@ -194,6 +203,7 @@
             $('#grand_totalInput').val(subTotal);
         });
     }
+
 </script>
 <script>
     $(document).ready(function() {
@@ -201,9 +211,9 @@
             var productId = $(this).val();
             // Make Ajax request
             $.ajax({
-                url: '/get-product-details/' + productId,
-                type: 'GET',
-                success: function(data) {
+                url: '/get-product-details/' + productId
+                , type: 'GET'
+                , success: function(data) {
                     // Update the code and sale_rate fields
                     $('input[name="code"]').val(data.code);
                     // Reset the sale_rate input to the original value from the server
@@ -212,8 +222,8 @@
                     // Reset the quantity input to 1
                     $('#quantityInput').val(1);
                     $('#total_price_b').val(data.sale_rate);
-                },
-                error: function(xhr, status, error) {
+                }
+                , error: function(xhr, status, error) {
                     console.error('Error fetching product details:', error);
                 }
             });
@@ -224,9 +234,9 @@
             var enteredCode = $(this).val();
             // Make Ajax request to fetch item details based on the entered code
             $.ajax({
-                url: '/get-product-details-by-code/' + enteredCode,
-                type: 'GET',
-                success: function(data) {
+                url: '/get-product-details-by-code/' + enteredCode
+                , type: 'GET'
+                , success: function(data) {
                     // $('.items').val(data.id).change();
                     $('.items').val(data.id).trigger('change');
                     $('input[name="original_sale_rate"]').val(data.sale_rate);
@@ -234,8 +244,8 @@
                     // Reset the quantity input to 1
                     $('#quantityInput').val(1);
                     $('#total_price_b').val(data.sale_rate);
-                },
-                error: function(xhr, status, error) {
+                }
+                , error: function(xhr, status, error) {
                     console.error('Error fetching product details by code:', error);
                 }
             });
@@ -281,6 +291,7 @@
             $('#grand_totalInput').val(discountedSubTotal.toFixed(1));
         }
     });
+
 </script>
 
 @endpush
